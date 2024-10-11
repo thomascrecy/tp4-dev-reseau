7e50d093659128e222e74f374680e6283c2f896f
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-11 11:31:42 +0200

    2

---
+++ bs_client_I2.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+
+# On définit la destination de la connexion
+host = '10.4.4.11'  # IP du serveur
+port = 13337        # Port choisir par le serveur
+
+# Création de l'objet socket de type TCP (SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connexion au serveur
+s.connect((host, port))
+# note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
+
+# Envoi de data bidon
+print('Que veux-tu envoyer au serveur :')
+message = input()
+s.sendall(message.encode('utf-8'))
+
+# On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
+data = s.recv(1024)
+
+# On libère le socket TCP
+s.close()
+
+# Affichage de la réponse reçue du serveur
+print(f"Le serveur a répondu {repr(data)}")
+
+try :
+    print(f'Connecté avec succès au serveur {host} sur le port {port}')
+except :
+    print('Ca a bug ptdrrr')
+
+sys.exit(0)
---
+++ bs_server_I2.py
@@ -0,0 +1,46 @@
+import socket
+
+# On choisit une IP et un port où on va écouter
+host = '10.4.4.11' # string vide signifie, dans ce conetxte, toutes les IPs de la machine
+port = 13337 # port choisi arbitrairement
+
+# On crée un objet socket
+# SOCK_STREAM c'est pour créer un socket TCP (pas UDP donc)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# On demande à notre programme de se bind sur notre port
+s.bind((host, port))  
+
+# Place le programme en mode écoute derrière le port auquel il s'est bind
+s.listen(1)
+# On définit l'action à faire quand quelqu'un se connecte : on accepte
+conn, addr = s.accept()
+# Dès que quelqu'un se connecte, on affiche un message
+print(f"Un client vient de se co et son IP c'est {addr}.")
+
+# Petite boucle infinie (bah oui c'est un serveur)
+# A chaque itération la boucle reçoit des données et les traite
+while True:
+
+    try:
+        # On reçoit 1024 bytes de données
+        data = conn.recv(1024)
+
+        # Si on a rien reçu, on continue
+        if not data: break
+
+        # On affiche dans le terminal les données reçues du client
+        print(f"Données reçues du client : {data}")
+
+        if "meo" in data:
+            conn.sendall(str.encode("Meo à toi confrère.", "utf-8"))
+        elif "waf" in data:
+            conn.sendall(b"ptdr t ki")
+        else:
+            conn.sendall(b"Mes respects humble humain.")
+
+    except socket.error:
+        print("Error Occured.")
+        break
+
+# On ferme proprement la connexion TCP
+conn.close()
