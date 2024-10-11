1f5f9247fca350ccd5dd0912f0f4e2ea3a328674
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-11 10:58:21 +0200

    server et client

--- bs_server_I1.py
+++ bs_server_I1.py
@@ -1,7 +1,7 @@
 import socket
 
 # On choisit une IP et un port où on va écouter
-host = '10.4.4.10' # string vide signifie, dans ce conetxte, toutes les IPs de la machine
+host = '10.4.4.11' # string vide signifie, dans ce conetxte, toutes les IPs de la machine
 port = 13337 # port choisi arbitrairement
 
 # On crée un objet socket
